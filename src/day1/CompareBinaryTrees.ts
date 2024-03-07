dd31cfdac78169e8f53a7fd7ee21025f0bf26b70
Eduardo Palhares <xeduardopalhares@gmail.com>
2024-03-07 19:31:40 -0300

    add compare binary trees with bfs

--- src/day1/CompareBinaryTrees.ts
+++ src/day1/CompareBinaryTrees.ts
@@ -1,3 +1,18 @@
 export default function compare(a: BinaryNode<number> | null, b: BinaryNode<number> | null): boolean {
+  // Structural Check
+  if (a === null && b === null) {
+    return true;
+  }
 
+  // Structural Check
+  if (a === null || b === null) {
+    return false;
+  }
+
+  // Value Check
+  if (a.value !== b.value) {
+    return false;
+  }
+
+  return compare(a.left, b.left) && compare(a.right, b.right);
 }
